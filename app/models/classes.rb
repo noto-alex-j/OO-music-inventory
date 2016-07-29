57a17fdd53489f59761af21a683782bf5a5a07cd
Alexander Noto <noto.alex.j@gmail.com>
2016-07-29 10:20:37 -0500

    starting from scratch

--- app/models/classes.rb
+++ app/models/classes.rb
@@ -1,8 +1,8 @@
 class Song
   def initialize(title,artist,album,genre,minute,second,songrating,albumrating)
     @title = title
-    @artist = artist #.split.map { |i| i.capitalize }.join(' ')
-    @album = album #.split.map { |i| i.capitalize }.join(' ')
+    @artist = artist 
+    @album = album
     @genre = genre
     @lengthmin = minute
     @lengthsec = second
@@ -17,4 +17,9 @@ class Song
     @artist = @artist.split.map { |i| i.capitalize }.join(' ')
     return "#{@title}||#{@artist}||#{@album}||#{@genre}||#{@lengthmin}||#{@lengthsec}||#{@songrating}||#{@albumrating}\n"
   end
+
+  def songAdd2File(songfile)
+   File.open('music_db.txt', 'a') {|file| file.puts "#{self.songfile()}" }
+  end
+
 end
