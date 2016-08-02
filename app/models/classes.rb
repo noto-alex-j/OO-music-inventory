21a144879ef4ea2af47e50cab13b9d05a4a11182
Alexander Noto <noto.alex.j@gmail.com>
2016-08-02 11:14:22 -0500

    artist find method

--- app/models/classes.rb
+++ app/models/classes.rb
@@ -10,16 +10,26 @@ class Song
     DB.execute("INSERT INTO songs (title,album_id,rating,length) VALUES ('#{@title}',#{@album},#{@rating},#{@length});")
   end
 
+  def self.find()
+
 end
 
 
 class Artist
-  def initialize(name)
-    @name = name
+  def initialize(name, id = nil)
+    @id = id
+    @name = name.downcase
   end
 
   def save
     DB.execute("INSERT INTO artists (name) VALUES ('#{@name}');")
+  end
+
+  def self.find(name)
+    name = name.downcase
+    record = DB.execute("SELECT * FROM artists WHERE name = '#{name}';")
+    record = record[0]
+    Artist.new(record["name"],record["id"])
   end
 
 end
--- test.rb
+++ test.rb
@@ -3,11 +3,38 @@ require 'sqlite3'
 DB = SQLite3::Database.new("music.db")
 DB.results_as_hash = true
 
+class Artist
+  def initialize(name, id = nil)
+    @id = id
+    @name = name.downcase
+  end
+
+  def save
+    DB.execute("INSERT INTO artists (name) VALUES ('#{@name}');")
+  end
+
+  def self.find(name)
+    name = name.downcase
+    record = DB.execute("SELECT * FROM artists WHERE name = '#{name}';")
+    record = record[0]
+    Artist.new(record["name"],record["id"])
+  end
+
+end
+
+# x = Artist.new("Gungor")
+# x.save
+stuff = Artist.find("Gungor")
+p stuff
 
 # DB.execute("INSERT INTO songs (title,artist_id,rating,length) VALUES ('Gold',32,5,4);")
 
-# DB.execute("INSERT INTO artists (name) VALUES ('Chet Faker');")
+# DB.execute("INSERT INTO artists (name) VALUES ('Glass Animals');")
 
-# puts DB.execute("SELECT * FROM songs;")
+# record = DB.execute("SELECT * FROM artists WHERE name = 'Gungor';")
+
+# p record[0]
 
 # DB.execute("DELETE FROM artists;")
+
+# p DB.execute("SELECT * FROM artists;")
